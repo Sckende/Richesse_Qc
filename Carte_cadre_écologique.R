63137957b27b4c10ae1a2c9cd57558d531c40f3e
Sckende <clrccljuhasz@gmail.com>
2020-10-30 11:11:31 -0400

    # correction des beugs dans carte cadre écologique # nouveau script pour analyses de rarefaction

--- Carte_cadre_écologique.R
+++ Carte_cadre_écologique.R
@@ -5,7 +5,7 @@ library(leaflet)
 library(sf)
 library(rgdal)
 library(rmapshaper)
-
+library(spatialEco)
 getwd()
 #### Chargement données LINUX ####
 # ---------- Chargement des polygones
@@ -18,7 +18,7 @@ occ <- readRDS("/home/claire/Bureau/PostDoc_COLEO/GitHub/Richesse_Qc/tmp.rds")
 #### Chargement données WINDOWS ####
 # ---------- Chargement des polygones
 CR01 <- rgdal::readOGR("C:/Users/HP_9470m/Desktop/PostDoc_COLEO/GitHub/Richesse_Qc/CERQ_SHP", layer = "CR_NIV_01_S", stringsAsFactors = FALSE, encoding = "UTF-8")
-CR01 <- rgdal::spTransform(CR01, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+CR01 <- sp::spTransform(CR01, CRS("+proj=longlat +datum=WGS84 +no_defs"))
 
 str(CR01, max.level = 3)
 
@@ -32,6 +32,9 @@ occ <- readRDS("C:/Users/HP_9470m/Desktop/PostDoc_COLEO/GitHub/Richesse_Qc/tmp.r
 # ----------  Création d'une fonction pour obtenir une couleur par province
 pal <- colorFactor("Dark2", domain = CR01$NOM_PROV_N)
 
+# ----------  Création d'une fonction pour obtenir une couleur par groupe d'espèces
+pal_esp <- colorFactor("Dark2", domain = unique(occ$species_gr))
+
 # ----------  Création des pop-ups par province
 labels <- sprintf( #Use C-style String Formatting Commands
   "<strong>%s</strong>",
@@ -40,14 +43,15 @@ labels <- sprintf( #Use C-style String Formatting Commands
 
 # ---------- Association des provinces naturelles avec les occurences
 # Intersection occurences et polygones
+coord_occ <- occ[,c(7,8)] 
 sp::coordinates(occ) <- ~lon+lat # création d'un objet de classe SpatialPointDataFrame
 proj4string(occ) <- "+proj=longlat +datum=WGS84 +no_defs" # Définir le CRS des occurences
 pts_in_poly <- spatialEco::point.in.poly(occ, CR01) # Association d'une province naturelle (ou NA si en dehors) et de tous ses attributs pour chaque occurence
 head(pts_in_poly@data)
 pts_in_poly@data$NOM_PROV_N <- as.factor(pts_in_poly@data$NOM_PROV_N)
 summary(pts_in_poly@data)
-
-occ_qc <- occ[!is.na(pts_in_poly@data$NOM_PROV_N),] # Retrait des occurences en dehors du Québec
+pts_in_poly@data <- cbind(pts_in_poly@data, coord_occ) # récupération des lat/long
+occ_qc <- pts_in_poly@data[!is.na(pts_in_poly@data$NOM_PROV_N),] # Retrait des occurences en dehors du Québec
 
 # ---------- Sous-selection du jeu de données des occurences
 # n_rows <- 50000
